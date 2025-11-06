8a28a31c3d83ec2148626c0dab00ba41a3220f2e
wangyidan <3627339340@qq.com>
2025-11-06 12:28:11 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,17 @@
+{
+  "name": "future-echo",
+  "version": "1.1.1",
+  "description": "未来回音 💌",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "path": "^0.12.7"
+  },
+  "engines": {
+    "node": ">=18"
+  },
+  "license": "MIT"
+}
